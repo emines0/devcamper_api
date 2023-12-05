1e8997bf9a46848357dda222f4e6c5406bf95026
emines0 <emin.sefa1@gmail.com>
2023-12-05 14:42:59 +0100

    remove courses if bootcamp removed

--- controllers/courses.js
+++ controllers/courses.js
@@ -23,22 +23,21 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     });
   }
 
+  // Execute query
+  const courses = await query;
+
+  res.status(200).json({
+    success: true,
+    count: courses.length,
+    data: courses,
+  });
+});
+
 // @desc    Get single courses
 // @route   GET /api/v1/courses
-// @route   GET /api/v1/bootcamps/:bootcampId/courses
+// @route   GET /api/v1/bootcamps/:id/
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  // If there is a bootcampId, find the courses for that bootcampId
-  let query;
-  if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  } else {
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description',
-    });
-  }
-
   // Execute query
   const courses = await query;
 

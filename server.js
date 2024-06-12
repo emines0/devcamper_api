35449bde63db95e3d027fdc6fdabf0bd9a3facdd
emines0 <emin.sefa1@gmail.com>
2024-06-12 19:02:02 +0200

    error handling middleware, async handler

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const errorHandler = require("./middleware/error");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -27,6 +28,9 @@ if (process.env.NODE_ENV === "development") {
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+
+// Error handler middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
 
